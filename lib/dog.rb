04f6830392109a70d8e4135ba3abf9a8af781fac
Max Bonus <mguzenko@outlook.com>
2020-04-04 01:19:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  def initialize(dog_name,bark)
+    @name = dog_name
+  end
+
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def name
+    @name
+  end
+end
