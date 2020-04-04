86d55a3a3616b8b26f6edac47f411655b93adbd0
Max Bonus <mguzenko@outlook.com>
2020-04-04 01:21:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Dog
-  def initialize(dog_name)
-    @name = dog_name
+  def initialize(name)
+    @name = name
   end
 
 
-  def name=(dog_name)
-    @name = dog_name
+  def name=(name)
+    @name = name
   end
 
   def bark
